2d84eabd9021700b5ab5f9444885726e3866e480
Tim Poerschke <info@coder-tim.de>
2020-06-15 20:04:21 +0200

    Keras-Model hinzugefügt

--- classifier-app.py
+++ classifier-app.py
@@ -1,6 +1,27 @@
+import os
+os.environ['KERAS_BACKEND'] = 'theano'
+
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route('/classify')
+@app.route('/classify', methods=["POST"])
 def classify():
-    return "Test"
+    image = request.files['image']
+
+    img_rows, img_cols = 28, 28
+
+    image = np.array(Image.open(image).convert('L'))
+    image = image.reshape(28 * 28)
+
+    model = load_model(os.path.join(os.curdir, "simple-mnist-model"))
+
+    prediction = model.predict_classes(np.array([image]))[0]
+
+    return {
+        "prediction": int(prediction)
+    }
